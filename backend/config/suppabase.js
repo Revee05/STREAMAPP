295553b8114d86665e6ee8c561dbaafb056b014a
Revee05 <Alfarezt05@gmail.com>
2021-06-27 00:45:03 +0700

    ,

---
+++ backend/config/suppabase.js
@@ -0,0 +1,8 @@
+// supabase.js
+import { createClient } from '@supabase/supabase-js'
+require('dotenv').config();
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
